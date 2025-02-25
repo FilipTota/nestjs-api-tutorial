feb17e7b04ac586a813f5b7ccf70dbbaa39bb3b9
filiptota <filip.tota5@gmail.com>
2025-02-25 15:07:31 +0100

    setup authentication for login and register, add auth dto, update db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,14 +9,14 @@ export class AuthController {
   // @desc    Register user
   // @route   POST /auth/register
   @Post('register')
-  register() {
-    return this.authService.register();
+  register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
   }
 
   // @desc    Login user
   // @route   POST /auth/login
   @Post('login')
-  login() {
-    return this.authService.login();
+  login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  register() {
-    return {
-      msg: 'Register',
-    };
+  async register(dto: AuthDto) {
+    // generate pass
+    const hashPassword = await argon.hash(dto.password);
+
+    try {
+      // save user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hashPassword,
+        },
+        // select is used to return selected values to user variable
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      // return saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  login() {
-    return {
-      msg: 'Login',
-    };
+  async login(dto: AuthDto) {
+    // find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Wrong credentials');
+    }
+
+    // compare password
+    const machedPassword = await argon.verify(user.password, dto.password);
+    if (!machedPassword) {
+      throw new ForbiddenException('Wrong credentials');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...userWithoutPassword } = user;
+
+    // return user
+    return userWithoutPassword;
   }
 }
